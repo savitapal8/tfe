cf4206fe476407c17c2ce3338fb759fc70e9f89e
Ashwin Naik <ashwinknaik@google.com>
2021-10-01 12:02:55 +0530

    chnage

--- main.tf
+++ main.tf
@@ -7,45 +7,30 @@ resource "google_service_account" "default" {
   display_name = "Service Account"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "n1-standard-4"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
 
   labels = {
-    "key" = "value"
+    foo = "bar"
+  }
+}
+
+
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret"
+
+  labels = {
+    label = "my-label"
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
   }
 }
