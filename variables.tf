4cb457449bbb30973ea0ae7186eb8899723b2508
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-10-14 17:30:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "access_token" {
   type        = string
   sensitive   = true
 }
+
+variable "project_id"{
+  description = "project_id"
+  type        = string
+  default     = "airline1-sabre-wolverine"
+}
+
+variable "region"{
+  description = "region"
+  type        = string
+  default     = "us-central1"
+}
