61dacfa7e86175d3ea2b63f300c24fb8d6b96150
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-10-14 17:29:07 +0530

    Update interconnect.tf

--- interconnect.tf
+++ interconnect.tf
@@ -1,29 +1,28 @@
+provider "google"{
+    region = var.region
+}
 
 resource "google_compute_router" "foobar" {
   name    = "router"
+  project = var.project_id
+  #region  = "us-central1"
   network = google_compute_network.foobar.name
   bgp {
     asn = 16550
   }
 }
 
-resource "google_compute_network" "foobar" {
-  name                    = "network"
-  auto_create_subnetworks = false
+resource "google_compute_network" "vpc_network" {
+  name     = "network"
+  project  = var.project_id
+  auto_create_subnetworks = false  
 }
 
-resource "google_compute_interconnect_attachment" "on_prem_1" {
+resource "google_compute_interconnect_attachment" "on_prem" {
   name                     = "on-prem-attachment"
-  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
-  type                     = "PARTNER"
-  router                   = google_compute_router.foobar.id
-}
-
-resource "google_compute_interconnect_attachment" "on_prem_2" {
-  name                     = "on-prem-attachment"
+  project                  = var.project_id
   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
   type                     = "PARTNER"
   router                   = google_compute_router.foobar.id
   mtu                      = 1500
-  region = "eu-central-1"
 }
