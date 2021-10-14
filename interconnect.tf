738e3d071480929462decae49e50c307c543ebfa
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-10-14 18:15:09 +0530

    Update interconnect.tf

--- interconnect.tf
+++ interconnect.tf
@@ -18,7 +18,7 @@ resource "google_compute_interconnect_attachment" "on_prem" {
   name                     = "on-prem-attachment"
   project                  = var.project_id 
   region                   = var.region
-  mtu                      = 1500
+  mtu                      = 1440
   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
   type                     = "PARTNER"
   router                   = google_compute_router.foobar.id
